34bb27e6e02b429c2b4b4d51c35e4745a27cd71d
Deng-Xian-Sheng <x17615848429@sina.com>
2022-06-23 11:02:34 +0800

    feat: test code file

--- test/pkg/api/subpackage/data_structures.go
+++ test/pkg/api/subpackage/data_structures.go
@@ -1,6 +1,7 @@
 package subpackage
 
 type SimpleStructure struct {
-	Id   int    `json:"id" swaggo:"true,the user id,2"`
-	Name string `json:"name" swaggo:",the user name,John Smith"`
+	Id    int    `json:"id" swaggo:"true,the user id,2"`
+	Name  string `json:"name" swaggo:",the user name,John Smith"`
+	Phone string `from:"phone" swaggo:"false,This is to test the from tag"`
 }
